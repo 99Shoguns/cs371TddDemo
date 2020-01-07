5911a66f6add30c92ffa0d88544d7880365ec290
Jude Battista <jbattista20@my.whitworth.edu>
2020-01-07 04:27:26 -0800

    Basic project for a Quadratic Equation solver to demonstrate TDD

---
+++ tddExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tddExample
+{
+    class Program
+    {
+        List<double> SolveQuadratic(double a, double b, double c) {
+        
+        }
+
+        void PrintAnswer(List<double> answer) { 
+        
+        }
+
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            List<double> answer = SolveQuadratic(a, b, c);
+            PrintAnswer(answer);
+            Console.ReadLine();
+
+        }
+    }
+}
